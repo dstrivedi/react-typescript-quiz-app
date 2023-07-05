1ca725e0d2a6eb8bc6f00285a0b4a867060244b2
dstrivedi <dstrivedi@users.noreply.github.com>
2023-07-05 14:28:58 -0400

    1 file modified, 3 added

---
+++ src/API.ts
@@ -0,0 +1,33 @@
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
+export type QuestionState = Question & { answers: string[] };
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+};
+
+export const fetchQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const url = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}`;
+  const result = await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      return data.results.map((question: Question) => ({
+        ...question,
+        answers: [...question.incorrect_answers, question.correct_answer],
+      }));
+    })
+    .catch((error) => console.log(error));
+  return result;
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,61 @@
 import * as React from 'react';
 import './style.css';
 
-export default function App() {
+import QuestionCard from './components/QuestionCard';
+
+import { fetchQuestions, Difficulty, QuestionState } from './API';
+
+const TOTAL_QUESTIONS: number = 10;
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correct_answer: string;
+};
+
+const App = () => {
+  const [questions, setQuestions] = React.useState<QuestionState[]>([]);
+  const [loading, setLoading] = React.useState(false);
+  const [number, setNumber] = React.useState(0);
+  const [userAnswers, setUserAnswers] = React.useState<AnswerObject[]>([]);
+  const [score, setScore] = React.useState(0);
+  const [gameOver, setGameOver] = React.useState(true);
+
+  console.log('Started ....');
+
+  const startQuiz = async () => {
+    setLoading(true);
+    setGameOver(false);
+
+    const newQuestion = await fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY);
+
+    setQuestions(newQuestion);
+    setScore(0);
+    setUserAnswers([]);
+    setNumber(0);
+    setLoading(false);
+  };
+
+  const checkAnswer = () => {};
+
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <div className="App">
+      <h1>Quiz App</h1>
+      <button className="start">Start</button>
+      <p className="score">Score:</p>
+      <p>Loading questions ... </p>
+      <QuestionCard
+        questionNum={number + 1}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        totalQuestions={TOTAL_QUESTIONS}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
+      <button className="next">Next question</button>
     </div>
   );
-}
+};
+
+export default App;
